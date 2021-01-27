5ddc891d5ad5abc1fce232cf50e816cdff8a8fa2
Dendraspis <dendraspis@gmx.net>
2021-01-28 00:07:50 +0100

    Fix limitReferences default value for Medium preset
    Full description:  #6

--- source/common/param.cpp
+++ source/common/param.cpp
@@ -240,7 +240,7 @@ void x265_param_default(x265_param* param)
     param->subpelRefine = 2;
     param->searchRange = 57;
     param->maxNumMergeCand = 3;
-    param->limitReferences = 1;
+    param->limitReferences = 3;
     param->limitModes = 0;
     param->bEnableWeightedPred = 1;
     param->bEnableWeightedBiPred = 0;
@@ -527,7 +527,6 @@ int x265_param_default_preset(x265_param* param, const char* preset, const char*
         {
             param->mcstfFrameRange = 1;
             param->maxNumMergeCand = 2;
-            param->limitReferences = 3;
             param->bIntraInBFrames = 0;
             param->lookaheadDepth = 15;
             param->bFrameAdaptive = 0;
@@ -541,7 +540,6 @@ int x265_param_default_preset(x265_param* param, const char* preset, const char*
         {
             param->mcstfFrameRange = 1;
             param->maxNumMergeCand = 2;
-            param->limitReferences = 3;
             param->bIntraInBFrames = 0;
             param->lookaheadDepth = 15;
             param->bFrameAdaptive = 0;
@@ -553,7 +551,6 @@ int x265_param_default_preset(x265_param* param, const char* preset, const char*
         {
             param->mcstfFrameRange = 1;
             param->maxNumMergeCand = 2;
-            param->limitReferences = 3;
             param->bEnableEarlySkip = 0;
             param->bIntraInBFrames = 0;
             param->lookaheadDepth = 15;
@@ -569,7 +566,6 @@ int x265_param_default_preset(x265_param* param, const char* preset, const char*
         }
         else if (!strcmp(preset, "slow"))
         {
-            param->limitReferences = 3;
             param->bEnableEarlySkip = 0;
             param->bIntraInBFrames = 0;
             param->bEnableRectInter = 1;
@@ -601,6 +597,7 @@ int x265_param_default_preset(x265_param* param, const char* preset, const char*
             param->searchMethod = X265_STAR_SEARCH;
             param->maxNumReferences = 5;
             param->limitModes = 1;
+            param->limitReferences = 1;
             param->lookaheadSlices = 0; // disabled for best quality
             param->limitTU = 4;
         }
