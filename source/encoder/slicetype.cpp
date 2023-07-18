856fb2e4f5df0794494fb6f3471f91f28a43fb85
Karam Singh <karam.singh@multicorewareinc.com>
2023-07-18 17:48:32 +0530

    Fix in zonefile parameter reflection in Lookahead analysis

--- source/encoder/slicetype.cpp
+++ source/encoder/slicetype.cpp
@@ -1975,14 +1975,6 @@ void Lookahead::slicetypeDecide()
             }
         }
         int j;
-		if (m_param->bResetZoneConfig)
-		{
-			for (int i = 0; i < m_param->rc.zonefileCount; i++)
-			{
-				if (m_param->rc.zones[i].startFrame == curFrame->m_poc)
-					m_param = m_param->rc.zones[i].zoneParam;
-			}
-		}
         for (j = 0; j < m_param->bframes + 2; j++)
         {
             if (!curFrame) break;
